527e0af2751e0dc8fd1150cb748a08d0006b3a60
Renan <Renan@DESKTOP-DJ18MHN>
2019-12-17 00:14:13 -0200

    coloquei o nome renan

--- GameEngine/src/com/bakerystudios/Main.java
+++ GameEngine/src/com/bakerystudios/Main.java
@@ -5,7 +5,7 @@ import com.bakerystudios.game.Game;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Testando");
+		System.out.println("Testando renan");
 		Game game = new Game();
 		game.start();
 	}
