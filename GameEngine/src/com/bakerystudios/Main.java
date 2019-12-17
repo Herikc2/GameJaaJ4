a5ed49850cf5f2c64a88ab7011484306c3c8430a
Herikc <herikcbrecher@outlook.com.br>
2019-12-17 00:20:56 -0200

    Commit testando Herikc

--- GameEngine/src/com/bakerystudios/Main.java
+++ GameEngine/src/com/bakerystudios/Main.java
@@ -5,7 +5,7 @@ import com.bakerystudios.game.Game;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Testando renan uedgfisdgufoh");
+		System.out.println("Testando herikc");
 		Game game = new Game();
 		game.start();
 	}
