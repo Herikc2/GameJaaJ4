f5409cb2a1343743f4b7bd6908f9497075592cf3
Herikc <herikcbrecher@outlook.com.br>
2019-12-16 23:49:58 -0200

    Commit teste

--- GameEngine/src/com/bakerystudios/Main.java
+++ GameEngine/src/com/bakerystudios/Main.java
@@ -5,6 +5,7 @@ import com.bakerystudios.game.Game;
 public class Main {
 	
 	public static void main(String[] args) {
+		System.out.println("Testando");
 		Game game = new Game();
 		game.start();
 	}
