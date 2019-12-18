2841404d3801803174a7a850b34dd9a61260b6c6
Renan <Renan@DESKTOP-DJ18MHN>
2019-12-17 22:25:27 -0200

    mudei aqui

--- GameEngine/src/com/bakerystudios/Main.java
+++ GameEngine/src/com/bakerystudios/Main.java
@@ -5,7 +5,7 @@ import com.bakerystudios.game.Game;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Testando herikc");
+		System.out.println("Testando herikc branch renan");
 		Game game = new Game();
 		game.start();
 	}
